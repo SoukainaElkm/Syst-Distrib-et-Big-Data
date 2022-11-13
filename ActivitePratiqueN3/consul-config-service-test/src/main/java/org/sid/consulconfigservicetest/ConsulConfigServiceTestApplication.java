b05eb29da5f41e28e14fcaf4258ccc94d7184f79
Soukaina <soukainaelkamouni00@gmail.com>
2022-11-13 14:24:27 +0100

    V2

---
+++ ActivitePratiqueN3/consul-config-service-test/src/main/java/org/sid/consulconfigservicetest/ConsulConfigServiceTestApplication.java
@@ -0,0 +1,33 @@
+package org.sid.consulconfigservicetest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@Configuration
+public class ConsulConfigServiceTestApplication {
+
+	@Autowired
+	AppConfig appConfig;
+	@Value("${user.username}")
+	private String username;
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulConfigServiceTestApplication.class, args);
+	}
+	@GetMapping("/config")
+	public AppConfig getConfig(){
+		return appConfig;
+	}
+	@GetMapping("/credentials")
+	public String getCredentials(){
+		return username;
+	}
+
+
+}
---
+++ ActivitePratiqueN3/consul-config-service-test/src/main/java/org/sid/consulconfigservicetest/Credentials.java
@@ -0,0 +1,9 @@
+package org.sid.consulconfigservicetest;
+
+import lombok.Data;
+
+@Data
+public class Credentials {
+    private String username;
+    private String password;
+}
